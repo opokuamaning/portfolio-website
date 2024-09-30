8289524aa4b79cdd703702e8102a7a23cc5f5262
Prince Opoku Amaning <princoprince94@gmail.com>
2024-09-30 13:53:12 +0000

    Create nav.jsx

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,10 +1,13 @@
-import { Box, Button, Stack, Text } from '@chakra-ui/react'
+import { Box, Button, Stack, Image } from '@chakra-ui/react'
 import React from 'react'
+import logo from '../../assets/logo.png'
 
 const Nav = () => {
   return (
     <Box display={'flex'} justifyContent={'space-between'} alignItems={'center'} padding={'14px 72px'} backgroundColor={'color.white'}>
-        <Text fontWeight={'900'} fontSize={'30px'} color={'color.HeadingColor'}>POA</Text>
+        <Stack>
+          <Image src={logo} alt={'logo'} h={8} />
+        </Stack>
         <Stack>
               <Button backgroundColor={'color.primary'} color={'color.white'}>View Photography Portfolio</Button>
         </Stack>
