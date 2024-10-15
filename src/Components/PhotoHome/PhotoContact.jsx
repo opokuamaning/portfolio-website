947aaba663e61d0ce278f614b349d6159048e071
Prince Opoku Amaning <princoprince94@gmail.com>
2024-10-15 18:58:42 +0000

    Update PhotoContact.jsx

--- src/Components/PhotoHome/PhotoContact.jsx
+++ src/Components/PhotoHome/PhotoContact.jsx
@@ -1,65 +1,19 @@
 import React from 'react';
-import {
-  Stack,
-  FormControl,
-  Select,
-  Input,
-  FormErrorMessage,
-  Box,
-  Textarea,
-  Button,
-  Text,
-} from '@chakra-ui/react';
-import { Formik, Form, Field } from 'formik';
-import * as Yup from 'yup';
+import { Stack, Box, Text } from '@chakra-ui/react';
 import { socialLinks } from '../Data/db';
 import PhotoContactForm from './PhotoContactForm';
 
 const ContactForm = () => {
-  // Define form validation schema
-  const validationSchema = Yup.object().shape({
-    fullName: Yup.string().required('Full Name is required'),
-    email: Yup.string().email('Invalid email address').required('Email is required'),
-    phoneNumber: Yup.string().required('Phone Number is required'),
-    projectType: Yup.string().required('Please select a project type'),
-    message: Yup.string().required('Message is required'),
-  });
-
-  const initialValues = {
-    fullName: '',
-    email: '',
-    phoneNumber: '',
-    projectType: '',
-    message: '',
-  };
-
-  const handleSubmit = async (values, { setSubmitting, resetForm }) => {
-    try {
-      const response = await fetch('https://formspree.io/f/xanywald', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(values),
-      });
-
-      if (response.ok) {
-        alert('Your message has been sent successfully!');
-        resetForm();
-      } else {
-        alert('Failed to send your message.');
-      }
-    } catch (error) {
-      console.error('Error:', error);
-    }
-    setSubmitting(false);
-  };
-
   return (
-    <Box padding={{ base: '28px', md: '80px 108px' }} display={'flex'} flexDirection={'column'} gap={'30px'}>
+    <Box
+      padding={{ base: '28px', md: '80px 108px' }}
+      display={'flex'}
+      flexDirection={'column'}
+      gap={'30px'}
+    >
       <Stack
         display={'flex'}
-        flexDirection={{base: 'column', md: 'row'}}
+        flexDirection={{ base: 'column', md: 'row' }}
         justifyContent={'space-between'}
         color={'color.HeadingColor'}
       >
